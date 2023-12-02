359cd4853302eaf13871ba5714468c92e9c63787
Tyuiu.ModenovaAP@outlook.com <fanriall@mail.ru>
2023-12-02 09:02:58 +0500

    Add project files.

---
+++ Tyuiu.ModenovaAP.Sprint5.Review.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ModenovaAP.Sprint5.Review.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ModenovaAP.Sprint5.Review.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c75421e-8ec0-4bb9-b4fe-0e07265b9fa0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ModenovaAP.Sprint5.Review.V16/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.ModenovaAP.Sprint5.Review.V16.Lib;
+
+namespace Tyuiu.ModenovaAP.Sprint5.Review.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Моденова А. П. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Моденова Анастасия Павловна  | ИИПб-23-3                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask7V0.txt (файл взять из архива    *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:DataSprint5 и        *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор символьных данных.Найти    *");
+            Console.WriteLine("* количество слов длиной восемь символов в заданной строке.               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V16.txt";
+            string pathsave = @"C:\Users\1\source\repos\Tyuiu.ModenovaAP.Sprint5.Review\Tyuiu.ModenovaAP.Sprint5.Review.V16\bin\Debug\OutPutFileTask.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            pathsave = ds.LoadDataAndSave(path);
+
+            Console.WriteLine(pathsave);
+            Console.ReadKey();
+        }
+    }
+}
